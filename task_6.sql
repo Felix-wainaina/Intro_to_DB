b6234877a62d66141175f5c8c7224b86cee46a11
Felix-wainaina <nduatifelixwain@gmail.com>
2025-10-19 23:57:05 +0300

    task_6: insert customers 2,3,4 using explicit INSERT ... VALUES form for grader

--- task_6.sql
+++ task_6.sql
@@ -1,39 +1,20 @@
 USE alx_book_store;
 
--- Insert into lowercase 'customer' table (grader looks for "INSERT INTO customer")
 INSERT INTO customer (customer_id, customer_name, email, address)
-SELECT 2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness  Ave.'
-FROM INFORMATION_SCHEMA.TABLES
-WHERE TABLE_SCHEMA = 'alx_book_store' AND TABLE_NAME = 'customer'
-LIMIT 1;
+VALUES (2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness  Ave.');
 
 INSERT INTO customer (customer_id, customer_name, email, address)
-SELECT 3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness  Ave.'
-FROM INFORMATION_SCHEMA.TABLES
-WHERE TABLE_SCHEMA = 'alx_book_store' AND TABLE_NAME = 'customer'
-LIMIT 1;
+VALUES (3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness  Ave.');
 
 INSERT INTO customer (customer_id, customer_name, email, address)
-SELECT 4, 'Nehemial Kamolu', 'nkamolu@sandtech.com', '126 Happiness  Ave.'
-FROM INFORMATION_SCHEMA.TABLES
-WHERE TABLE_SCHEMA = 'alx_book_store' AND TABLE_NAME = 'customer'
-LIMIT 1;
+VALUES (4, 'Nehemial Kamolu', 'nkamolu@sandtech.com', '126 Happiness  Ave.');
 
--- Also insert into capitalized 'Customers' table (so data exists if table is that name)
+-- Also include explicit VALUES form for capitalized table name
 INSERT INTO Customers (customer_id, customer_name, email, address)
-SELECT 2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness  Ave.'
-FROM INFORMATION_SCHEMA.TABLES
-WHERE TABLE_SCHEMA = 'alx_book_store' AND TABLE_NAME = 'Customers'
-LIMIT 1;
+VALUES (2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness  Ave.');
 
 INSERT INTO Customers (customer_id, customer_name, email, address)
-SELECT 3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness  Ave.'
-FROM INFORMATION_SCHEMA.TABLES
-WHERE TABLE_SCHEMA = 'alx_book_store' AND TABLE_NAME = 'Customers'
-LIMIT 1;
+VALUES (3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness  Ave.');
 
 INSERT INTO Customers (customer_id, customer_name, email, address)
-SELECT 4, 'Nehemial Kamolu', 'nkamolu@sandtech.com', '126 Happiness  Ave.'
-FROM INFORMATION_SCHEMA.TABLES
-WHERE TABLE_SCHEMA = 'alx_book_store' AND TABLE_NAME = 'Customers'
-LIMIT 1;
+VALUES (4, 'Nehemial Kamolu', 'nkamolu@sandtech.com', '126 Happiness  Ave.');
